5788887e6257a2181ce0c98d0e29fb8478ca9475
Egger Dominik <egger.dominik1@gmail.com>
2019-10-03 22:48:20 +0200

    Implementation of GET_CALENDAR structure
    Implementation of GET_CALENDAR structure in gqlData.js

--- src/utilities/GitHubUtils/gqlData.js
+++ src/utilities/GitHubUtils/gqlData.js
@@ -56,3 +56,183 @@ export const GET_PROFILE = gql`
   }
 }
 `;
+
+// Structure GQL Calendar Code
+const GET_CALENDAR_QUERY_PART = (year,c) => {
+  return `
+  c${c}: contributionsCollection(to:"${year}"){
+     contributionCalendar{
+        totalContributions
+        weeks{
+          contributionDays{
+            contributionCount
+            date
+            color
+          }
+        }
+      }
+      commitContributionsByRepository {
+        contributions{
+          totalCount
+          
+        }
+        url
+        repository {
+          defaultBranchRef{
+            target{
+              ... on Commit{
+                changedFiles
+                additions
+                deletions
+                committedDate
+                history{
+                  edges{
+                    node{
+                      committedDate
+                    }
+                  }
+                }
+              }
+            }
+          }
+          name
+          nameWithOwner
+          url
+          owner{
+            avatarUrl
+            login
+            url
+          }
+          languages(first: 50, orderBy: {field: SIZE, direction: DESC}) {
+            totalCount
+            totalSize
+            edges {
+              size
+              node {
+                name
+                color
+              }
+            }
+          }
+        }
+      }
+      issueContributionsByRepository {
+        contributions{
+          totalCount
+        }
+        repository {
+          defaultBranchRef{
+            target{
+              ... on Commit{
+                changedFiles
+                additions
+                deletions
+                committedDate
+                history{
+                  edges{
+                    node{
+                      committedDate
+                    }
+                  }
+                }
+              }
+            }
+          }
+          name
+          nameWithOwner
+          url
+          owner{
+            avatarUrl
+            login
+            url
+          }
+          languages(first: 50, orderBy: {field: SIZE, direction: DESC}){
+            totalCount
+            totalSize
+            edges {
+              size
+              node {
+                name
+                color
+              }
+            }
+          }
+        }
+      }
+      pullRequestContributionsByRepository {
+        contributions{
+          totalCount
+        }
+        repository {
+          defaultBranchRef{
+            target{
+              ... on Commit{
+                changedFiles
+                additions
+                deletions
+                committedDate
+                history{
+                  edges{
+                    node{
+                      committedDate
+                    }
+                  }
+                }
+              }
+            }
+          }
+          name
+          nameWithOwner
+          url
+          owner{
+            avatarUrl
+            login
+            url
+          }
+          languages(first: 50, orderBy: {field: SIZE, direction: DESC}){
+            totalCount
+            totalSize
+            edges {
+              size
+              node {
+                name
+                color
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+};
+
+// Dynamic generate calenders structure
+const generate_calenders_query = (username, createdAtDate) => {
+
+  const date = new Date();
+  var query = ``;
+  var count = 1;
+
+  while(date.getFullYear() >= createdAtDate.getFullYear()){
+      
+      query += GET_CALENDAR_QUERY_PART(date.toJSON(),count);
+
+      date.setFullYear(date.getFullYear()-1);
+      count++;
+  }
+  return query;
+
+};
+
+// Get Calendar skeletal structure
+export const GET_CALENDAR = (username, createdAt) => {
+  const query = gql`
+                  query
+                  {
+                      user(login: "${username}") {
+                          ${generate_calenders_query(username, createdAt)}
+                      }
+                  }
+  `
+  return query;
+};
