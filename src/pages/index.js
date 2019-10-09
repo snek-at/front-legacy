12d073987bf2dc643b578371a6b1d10d6ad2c578
Luca Egger <lucaegger00@gmail.com>
2019-10-09 18:21:50 +0200

    Add Profile page
    Profile component has been added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 //> Components
 // Import all components to export them for easy access from parent components
 import SettingsPage from './Settings';
+import ProfilePage from './Profile';
 
 export {
     SettingsPage,
+    ProfilePage,
 }
