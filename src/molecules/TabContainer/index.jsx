0e5823eb5c49cbab3ff5ae26467085052ff62afd
Aichnerc <contact@aichner-christian.com>
2019-10-09 18:19:23 +0200

    Impove TabContainer
    The TabContainer now works as intended
    with its subcontainers.

--- src/molecules/TabContainer/index.jsx
+++ src/molecules/TabContainer/index.jsx
@@ -22,22 +22,22 @@ import './tabcontainer.scss';
 class TabContainer extends React.Component {
 
     state = {
-        activeItemOuterTabs: 0,
         activeItemInnerPills: 0,
+        activeSubItem: 0,
     };
     
-    toggleOuterTabs = tab => e => {
-        if (this.state.activeItemOuterTabs2 !== tab) {
+    toggleOuterPills = tab => e => {
+        if (this.state.activeItemInnerPills !== tab) {
         this.setState({
-            activeItemOuterTabs: tab
+            activeItemInnerPills: tab
         });
         }
     };
     
-    toggleInnerPills = tab => e => {
-        if (this.state.activeItemInnerPills !== tab) {
+    toggleSub = tab => e => {
+        if (this.state.activeSubItem !== tab) {
         this.setState({
-            activeItemInnerPills: tab
+            activeSubItem: tab
         });
         }
     };
@@ -54,7 +54,7 @@ class TabContainer extends React.Component {
                                     <MDBNavLink 
                                     to="#" 
                                     active={ this.state.activeItemInnerPills === key} 
-                                    onClick={this.toggleInnerPills(key)}
+                                    onClick={this.toggleOuterPills(key)}
                                     className="text-left"
                                     >
                                     <MDBIcon icon={setting.icon} className="mr-2" /> {setting.title}
@@ -66,11 +66,32 @@ class TabContainer extends React.Component {
                     </MDBCol>
                     <MDBCol md="9">
                         <MDBTabContent activeItem={this.state.activeItemInnerPills}>
-                        {this.props.settings.map((setting, key) => {
+                        {this.props.settings && this.props.settings.map((setting, key) => {
                             return(
-                                <MDBTabPane tabId={key} key={key}>
-                                    {setting.title}
-                                </MDBTabPane>
+                                <>
+                                    <MDBTabPane tabId={key} key={key}>
+                                        <MDBNav className="nav-tabs mt-5">
+                                        {setting.subtitles && setting.subtitles.map((subtitle, subkey) => {
+                                            return(
+                                                <MDBNavItem>
+                                                    <MDBNavLink
+                                                    to="#"
+                                                    active={this.state.activeSubItem === subkey}
+                                                    onClick={this.toggleSub(subkey)}
+                                                    role="tab" >
+                                                    {subtitle.title}
+                                                    </MDBNavLink>
+                                                </MDBNavItem>
+                                            )
+                                        })}
+                                        </MDBNav>
+                                        <MDBTabContent activeItem={this.state.activeSubItem} >
+                                            {/* 
+                                                To be added
+                                            */}
+                                        </MDBTabContent>
+                                    </MDBTabPane>
+                                </>
                             )
                         })}
                         </MDBTabContent>
@@ -80,36 +101,8 @@ class TabContainer extends React.Component {
         } else if(this.props.horizontal){
             return (
             <>
-                <ul class="nav nav-tabs" id="myTab" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"
-                    aria-selected="true">Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="something-tab" data-toggle="tab" href="#something" role="tab" aria-controls="something"
-                    aria-selected="false">Something</a>
-                </li>
-                </ul>
-                <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                    Raw denim you probably haven't heard of them jean shorts Austin.
-                    Nesciunt tofu stumptown aliqua, retro synth master leanse.
-                    Mustache cliche tempor, williamsburg carles vegan helvetica.
-                    Reprehenderit butcher retro keffiyeh dreamcatcher synth.
-                    Cosby sweater eu banh mi, qui irure terry richardson ex squid.
-                    Aliquip placeat salvia cillum iphone.
-                    Seitan aliquip quis cardigan american apparel, butcher voluptate nisiqui.
-                </div>
-                <div class="tab-pane fade" id="something" role="tabpanel" aria-labelledby="something-tab">Food truck fixie
-                    locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit,
-                    blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.
-                    Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum
-                    PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS
-                    salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit,
-                    sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester
-                    stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</div>
-                </div>
-                </>
+                <p>Not available yet</p>
+            </>
             );
         } else {
             return null
