22bf06523884aeff3707fd090c6a5c2532279741
Christian Aichner <contact@aichner-christian.com>
2019-10-09 22:22:43 +0200

    Improve stability
    The stability of the SocialData component has been
    improved by having improved if statements to check
    for non-existent data.
    Furthermore the "connected accounts" section has
    been moved to the Settings component.

--- src/components/molecules/Socialdata/index.jsx
+++ src/components/molecules/Socialdata/index.jsx
@@ -5,82 +5,73 @@ import React from 'react';
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
 import { 
-    MDBCardBody,
-    MDBIcon,
+  MDBCardBody,
+  MDBIcon,
 }  from 'mdbreact';
 
 //> CSS
 import './socialdata.scss';
 
 class Socialdata extends React.Component {
-    render() {
+  render() {
 
-    const {
-        status,
-        name,
-        company,
-        location,
-        email,
-        website,
-        accounts,
-    } = this.props;
+  const {
+      status,
+      name,
+      company,
+      location,
+      email,
+      website,
+      accounts,
+  } = this.props;
 
-    return (
-        <MDBCardBody id="socialdata">
-        <h4 className="font-weight-bold mb-4">{name}</h4>
-        {status &&
-            <>
-            <hr />
-            <div dangerouslySetInnerHTML={{ __html: status.emojiHTML}}></div>
-            <p className="dark-grey-text">
-                {status.message}
-            </p>
-            </>
-        }
-        {(company || location || email || website) &&
-            <hr />
-        }
-        <div className="vcard text-left">
-            {company &&
-            <span><MDBIcon icon="building"/>{company}</span>
-            }
-            {location &&
-            <span><MDBIcon icon="map-marker-alt"/>{location}</span>
-            }
-            {email &&
-            <a
-            href={"mailto:"+email}
-            ><span><MDBIcon icon="envelope"/>{email}</span>
-            </a>
-            }
-            {website &&
-            <a
-            href={"https://"+website}
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            <span><MDBIcon icon="link"/>{website}</span>
-            </a>
-            }
-            {(accounts.github || accounts.gitlab) &&
-            <div className="text-center">
-            <hr />
-            <p className="mb-2">Connected accounts</p>
-            <div className="connected">
-            
-                {accounts.github &&
-                <MDBIcon fab icon="github" size="2x" className="ml-2 mr-2" />
-                }
-                {accounts.gitlab &&
-                <MDBIcon fab icon="gitlab" size="2x" className="ml-2 mr-2" />
-                }
-            </div>
-            </div>
-            }        
-        </div>
-        </MDBCardBody>
-    );
+  console.log(status);
+
+  return (
+    <MDBCardBody id="socialdata">
+    <h4 className="font-weight-bold mb-4">{name}</h4>
+    {(status.message || status.emojiHTML) &&
+      <>
+      <hr />
+      {status.emojiHTML &&
+          <div dangerouslySetInnerHTML={{ __html: status.emojiHTML}}></div>
+      }
+      {status.message &&
+          <p className="dark-grey-text">
+              {status.message}
+          </p>
+      }
+      </>
     }
+    {(company || location || email || website) &&
+      <hr />
+    }
+    <div className="vcard text-left">
+      {company &&
+      <span><MDBIcon icon="building"/>{company}</span>
+      }
+      {location &&
+      <span><MDBIcon icon="map-marker-alt"/>{location}</span>
+      }
+      {email &&
+      <a
+      href={"mailto:"+email}
+      ><span><MDBIcon icon="envelope"/>{email}</span>
+      </a>
+      }
+      {website &&
+      <a
+      href={"https://"+website}
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+      <span><MDBIcon icon="link"/>{website}</span>
+      </a>
+      }     
+    </div>
+    </MDBCardBody>
+  );
+  }
 }
 export default Socialdata;
 
