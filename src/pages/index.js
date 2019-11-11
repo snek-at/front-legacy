e2b0b7f6ad05edc9f7549b7e96f5b3d7e862f938
Luca Egger <lucaegger00@gmail.com>
2019-11-11 09:36:32 +0100

    Add Profile page
    Profile component has been added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 //> Components
 // Import all components to export them for easy access from parent components
 import SettingsPage from "./Settings";
+import ProfilePage from "./Profile";
 
-export { SettingsPage };
+export { SettingsPage, ProfilePage };
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
