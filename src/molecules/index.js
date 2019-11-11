4af86278411c8a9085d0f4f8599cbfa96854086d
Luca Egger <lucaegger00@gmail.com>
2019-11-11 10:16:35 +0100

    Add molecule components
    Molecule components have been added from concept.

--- src/molecules/index.js
+++ src/molecules/index.js
@@ -1,8 +1,11 @@
 //> Components
 // Import all components to export them for easy access from parent components
 import TabContainer from "./TabContainer";
+import Avatar from "./Avatar";
+import Socialdata from "./Socialdata";
+import Calendar2D from "./Calendar2D";
 
-export { TabContainer };
+export { TabContainer, Avatar, Socialdata, Calendar2D };
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
