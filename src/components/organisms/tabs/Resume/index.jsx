e6acad27c9abb380c72fb7db43d468bef27ca155
Aichnerc <contact@aichner-christian.com>
2019-11-11 11:09:01 +0100

    Add components folder
    Fix of the violation of code quality guidelines.
    There was no components folder in the first
    place, so all components have been generated
    in the src folder, which is very unclean. It is now
    on @sweggerat to fix the paths.

--- src/components/organisms/tabs/Resume/index.jsx
+++ src/components/organisms/tabs/Resume/index.jsx
@@ -1,26 +1,20 @@
 //> React
 // Contains all the functionality necessary to define React components
-import React from 'react';
+import React from "react";
 
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
-import {
-    MDBTabPane,
-} from 'mdbreact';
+import { MDBTabPane } from "mdbreact";
 
 class Resume extends React.Component {
-    
-    render(){
-        return(
-            <MDBTabPane tabId={this.props.id} role="tabpanel">
-            </MDBTabPane>
-        )
-    }
+  render() {
+    return <MDBTabPane tabId={this.props.id} role="tabpanel"></MDBTabPane>;
+  }
 }
 
 export default Resume;
 
-/** 
+/**
  * SPDX-License-Identifier: (EUPL-1.2)
  * Copyright © 2019 Werbeagentur Christian Aichner
  */
