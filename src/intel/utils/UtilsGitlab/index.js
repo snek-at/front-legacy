da6b17893c7297d8c67af50a275210b87d003605
Egger Dominik <egger.dominik1@gmail.com>
2019-11-12 12:51:13 +0100

    Fill organization table with data
    The platform table is filled with data of a specific user
    now.

--- src/intel/utils/UtilsGitlab/index.js
+++ src/intel/utils/UtilsGitlab/index.js
@@ -52,12 +52,57 @@ const fillPlatform = async user => {
     emojiHTML
   ]);
 };
+
+const fillOrganizations = async (user) => {
+  const url = `https://${user.server}/users/${user.username}/groups.json`;
+  const html = await webscrap.parseJsonToDOM(
+    webscrap.fetchJson(url).then(html => {
+      return html;
+    })
+  );
+
+  const rows = html.getElementsByClassName("group-row");
+
+  for (const _org of Array.from(rows)) {
+    const avatarUrl = _org
+      .getElementsByClassName("avatar")[0]
+      .getAttribute("data-src");
+    const name = _org
+      .getElementsByClassName("group-name")[0]
+      .getAttribute("href").substring(1);
+    let orgUrl = `https://${user.server}/${name}`;
+
+    if (avatarUrl) {
+      if (!avatarUrl.includes("https://") || !avatarUrl.includes("http://")) {
+        avatarUrl = `https://${user.server}/${avatarUrl.substring(
+          1
+        )}`;
+      }
+    }
+
+    db.exec(insert.organization, [
+      avatarUrl,
+      name,
+      orgUrl
+    ]);
+    
+    var id_platform = db.exec('SELECT id FROM platform').pop()["id"]
+    var id_organization = db.exec('SELECT id FROM organization').pop()["id"]
+
+    db.exec(insert.platformHasOrganization, [
+      id_platform,
+      id_organization
+    ]);
+
   }
 };
 
 //> Export functions
 // Fill the Database from user object
+
 export const fill = (_db, user) => {
   db = _db;
-  return fillPlatform(user);
+  return fillPlatform(user).then(async()=>
+    await fillOrganizations(user)
+  );
 };
