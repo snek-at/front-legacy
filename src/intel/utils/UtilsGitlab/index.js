ab1e1d325750ad4f58a1b28f63d50eb055543fc9
Egger Dominik <egger.dominik1@gmail.com>
2019-11-12 11:51:05 +0100

    Fill platform table with data
    The platform table is filled with data of a specific user
    now.

--- src/intel/utils/UtilsGitlab/index.js
+++ src/intel/utils/UtilsGitlab/index.js
@@ -2,6 +2,56 @@ import * as insert from "../../Database/Statements/Insert";
 import * as webscrap from "../UtilsWebscrap";
 let db;
 
+//> Fill functions
+// Fill the platform table with data
+const fillPlatform = async user => {
+  const url = `https://${user.server}/${user.username}`;
+  const html = await webscrap.parseTextToDOM(
+    webscrap.fetchHtml(url).then(html => {
+      return html;
+    })
+  );
+  const status = html.getElementsByTagName("gl-emoji")[0];
+  const coverDesc = html
+    .getElementsByClassName("cover-desc")[0]
+    .getElementsByTagName("span");
+
+  const coverTitle = html.getElementsByClassName("cover-title")[0];
+  const avatarUrl = html
+    .getElementsByClassName("avatar-holder")[0]
+    .getElementsByTagName("a")[0]
+    .getAttribute("href");
+  const links = html.getElementsByClassName("profile-link-holder")[0];
+  const message = null;
+  const emojiHTML = null;
+  const fullName = coverDesc[0].innerHTML.trim().substring(1);
+  const date = coverDesc[1].innerHTML;
+
+  var dbAvatarURL = null;
+  if (avatarUrl.includes("https://") || avatarUrl.includes("http://")) {
+    dbAvatarURL = avatarUrl;
+  } else {
+    dbAvatarURL = `https://${user.server}/${avatarUrl.substring(1)}`;
+
+    const createdAt = new Date(date);
+
+    db.exec(insert.platform, [
+      user.platformName,
+      url,
+      dbAvatarURL,
+      null,
+      null,
+      null,
+      user.username,
+      fullName,
+      createdAt,
+      null,
+      message,
+      emojiHTML
+    ]);
+  }
+};
+
 //> Export functions
 // Fill the Database from user object
 export const fill = (_db, user) => {
