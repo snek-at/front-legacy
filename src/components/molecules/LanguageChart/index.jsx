c1a6c8ddf58a959b94d51cc4c332aea666d6f6f5
Christian Aichner <contact@aichner-christian.com>
2019-11-13 02:53:49 +0100

    Add polar radar chart
    The radar chart will display the used technology in
    programming languages.

---
+++ src/components/molecules/LanguageChart/index.jsx
@@ -0,0 +1,58 @@
+//> React
+// Contains all the functionality necessary to define React components
+import React from "react";
+
+//> Chart.js
+// "Material Design for Bootstrap" is using chart.js to generate its charts
+import { Polar } from "react-chartjs-2";
+
+class LanguageChart extends React.Component {
+  state = {
+    dataPolar: {
+      datasets: [
+        {
+          data: [300, 250, 100, 40],
+          backgroundColor: [
+            "rgba(247, 70, 74, 0.8)",
+            "rgba(70, 191, 189, 0.8)",
+            "rgba(253, 180, 92, 0.8)",
+            "rgba(148, 159, 177, 0.8)"
+          ],
+          label: "Your top languages"
+        }
+      ],
+      labels: ["JavaScript", "Python", "CSS", "Roff"]
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <p className="lead font-weight-bold text-center mt-3">
+          Your top languages
+        </p>
+        <Polar
+          className="w-100 h-auto"
+          data={this.state.dataPolar}
+          options={
+            ({
+              responsive: true
+            },
+            {
+              legend: {
+                display: false
+              }
+            })
+          }
+        />
+      </>
+    );
+  }
+}
+
+export default LanguageChart;
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -5,5 +5,18 @@ import Avatar from "./Avatar";
 import Socialdata from "./Socialdata";
 import Calendar2D from "./Calendar2D";
 import Calendar3D from "./Calendar3D";
+import LanguageChart from "./LanguageChart";
 
-export { TabContainer, Avatar, Socialdata, Calendar2D, Calendar3D };
+export {
+  TabContainer,
+  Avatar,
+  Socialdata,
+  Calendar2D,
+  Calendar3D,
+  LanguageChart
+};
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
