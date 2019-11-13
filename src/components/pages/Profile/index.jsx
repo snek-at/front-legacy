4075a1597f25884e0a816737cd347d641e7da677
Christian Aichner <contact@aichner-christian.com>
2019-11-13 03:05:18 +0100

    Improve code quality
    The code quality has been improved because of a
    request of @kleberbaum.

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -36,6 +36,7 @@ import {
 } from "../../organisms/tabs";
 
 //> Handlers
+// To be added
 
 //> CSS
 import "./profile.scss";
@@ -70,10 +71,77 @@ const tabitems = [
 ];
 
 class Dashboard extends React.Component {
+  state = {
+    data: undefined
+  };
+
+  componentDidMount = () => {
+    this.fetchData();
+  };
+
+  fetchData = async () => {
+    let data = localStorage.getItem("DUMMY_DATA");
+    if (data) {
+      let dataJSON = JSON.parse(data);
+      if (dataJSON) {
+        this.setState({
+          data: dataJSON
+        });
+      }
+    }
+  };
+
   render() {
-    return null;
+    const { username } = this.props.match.params;
+
+    // Debugging access point - get username from router
+    //console.log("User via GET param: "+username);
+
+    const { data } = this.state;
+
+    // Debugging access point - state
+    //console.log(this.state);
+
+    if (data) {
+      return (
+        <div id="profile">
+          <MDBContainer className="pt-5">
+            <MDBRow>
+              <MDBCol md="4" className="text-center">
+                <MDBCard testimonial>
+                  <MDBCardUp color="info" />
+                  <Avatar url={data.avatarUrl} alt={data.name} />
+                  <Socialdata
+                    status={data.status}
+                    name={data.name}
+                    company={data.company}
+                    location={data.location}
+                    email={data.email}
+                    website={data.websiteUrl}
+                    accounts={{
+                      github: data
+                    }}
+                  />
+                </MDBCard>
+              </MDBCol>
+              <MDBCol md="8">
+                <TabContainer items={tabitems} horizontal>
+                  <OverviewTab id={0} contributions={data.contributions} />
+                  <ResumeTab id={1} />
+                  <ProjectsTab id={2} repos={data.repos} />
+                  <EducationTab id={3} />
+                </TabContainer>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
+
 export default Dashboard;
 
 /**
