f5606994d2acc9e5eaa80c4613326f3be3da0537
Pinterid <david.pinterics@gmail.com>
2019-11-15 00:46:06 +0100

    Add Random Generator
    The state of an OAuth request is now a random string.

--- src/intel/OAuthGithub/providers/github.ts
+++ src/intel/OAuthGithub/providers/github.ts
@@ -1,8 +1,9 @@
+import { guid } from '../services/utilities'
 import { IProvider } from 'react-very-simple-oauth'
 
 const client_id = process.env.REACT_APP_GITHUB_CLIENT_ID;
 const client_secret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
-const state = "xxx"
+const state = guid()
 const redirect_uri = encodeURIComponent(`https://snek.at/oauth`)
 const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 
