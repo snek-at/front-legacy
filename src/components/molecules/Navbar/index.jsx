9bf5b96e546af0ae0d9d294978c5e7a1b9f50f0d
Aichnerc <contact@aichner-christian.com>
2019-11-15 01:59:47 +0100

    Improve code quality
    A pair of single line quotes has been replaced with
    double quotes.

--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -2,7 +2,7 @@
 // Contains all the functionality necessary to define React components
 import React from "react";
 // Router
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
