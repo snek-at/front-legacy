6c170017a3188a5d4f36fcaeac0aa48d1862db35
schettn <nicoschett@icloud.com>
2019-11-17 03:08:40 +0100

    Implement converter in intel
    Now it is possible to call the converter functions through intel.

--- src/intel/index.js
+++ src/intel/index.js
@@ -1,10 +1,45 @@
 //import * as data from "./IntelData";
 import * as data from "./IntelDummy";
 
-export function fill(user) {
-  data.fill(user);
+import * as converter from "./Database/Converter";
+
+export async function fill(user) {
+  await data.fill(user);
 }
 
-export function templateGetPlatform() {
-  return data.exec("SELECT * FROM platform");
-}
+export function templateSelect() {
+  return data.exec("SELECT * FROM repository");
+};
+
+export function user() {
+  return converter.getUser(data);
+};
+
+export function repos() {
+  return converter.getRepositories(data);
+};
+
+export function orgs() {
+  return converter.getOrganizations(data);
+};
+
+export function languages() {
+  return converter.getLanguages(data);
+};
+
+export function calendar() {
+  return converter.getCalendar(data);
+};
+
+export function stats() {
+  return converter.getStats(data);
+};
+
+export function contribTypes() {
+  return converter.getContribTypes(data);
+};
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
