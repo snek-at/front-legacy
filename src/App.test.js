b0077da33d50ae77239d9cb9e43aa1ee0f48274e
schettn <nicoschett@icloud.com>
2019-11-17 03:25:36 +0100

    Add testcase
    Now a test-case is added to check the functionality of intel.

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,15 @@ import * as intel from "./intel";
 intel.fill({
   name: "torvalds",
   platformName: "gitlab",
-  server: "gitlab.htl-villach.at"
+  server: "gitlab.htl-villach.at",
 }).then(() => {
+  console.log(intel.templateSelect());
   console.log(intel.user());
   console.log(intel.repos());
   console.log(intel.orgs());
   console.log(intel.languages());
   console.log(intel.calendar());
+  console.log(intel.stats());
+  console.log(intel.contribTypes());
 });
 
