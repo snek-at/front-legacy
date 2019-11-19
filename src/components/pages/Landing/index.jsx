f804f227041a39966e19e9cfbb3164f8c2466fd4
Aichnerc <contact@aichner-christian.com>
2019-11-19 21:05:30 +0100

    Implement TextTypist
    The self writing text has been implemented.

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -7,19 +7,39 @@ import React from "react";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 //> Components
-import { Register } from "../../organisms/sections";
+import {
+  TextTypist,
+} from "../../molecules";
+
+//> Components
+import { 
+  Register
+} from "../../organisms/sections";
 
 //> CSS
 import "./landing.scss";
+
+//> Typist Data
+const scope = [
+  "engineers",
+  "programmers",
+  "developers",
+  "entrepreneurs",
+  "students",
+  "teachers",
+  "sneks"
+];
 
 class Landing extends React.Component {
   render() {
     return (
       <div id="landing" className="py-5">
         <MDBContainer>
-          <MDBRow className="flex-center">
-            <MDBCol md="6">
-              <h1>Built for engineers</h1>
+          <MDBRow className="">
+            <MDBCol md="6" className="pt-5">
+              <h1 className="mt-5">
+              Built for <TextTypist scope={scope} />
+              </h1>
             </MDBCol>
             <MDBCol md="6">
               <Register />
