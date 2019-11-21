63a548fddaf846c6c90320d448311101a843859d
Pinterid <david.pinterics@gmail.com>
2019-11-21 08:48:04 +0100

    Add Basic Layout
    Added the basic layout for gitlab.

--- src/components/pages/GitLab/index.jsx
+++ src/components/pages/GitLab/index.jsx
@@ -2,9 +2,56 @@
 // Contains all the functionality necessary to define React components
 import React from "react";
 
+//> MDB
+// "Material Design for Bootstrap" is a great UI design framework
+import {
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardFooter,
+  MDBListGroup,
+  MDBListGroupItem,
+  MDBCardText,
+  MDBInput,
+  MDBBtn,
+  MDBBadge,
+  MDBTooltip,
+  MDBPopover,
+  MDBPopoverHeader,
+  MDBPopoverBody,
+  MDBIcon,
+} from "mdbreact";
+
 class GitLab extends React.Component {
   render() {
-    return null;
+    return(
+      <MDBCard id="gitlab" className="text-dark">
+      <MDBCardBody>
+        <h2>GitLab</h2>
+        <MDBInput
+        label="Username"
+        type="text"
+        name="username"
+        outline
+        size="lg"
+        />
+        <MDBInput 
+        label="Server"
+        type="text"
+        name="server"
+        outline
+        size="lg"
+        />
+        <MDBBtn
+        color="success"
+        className="w-100 py-3 font-weight-bold mx-0"
+        >
+        Sign in
+        </MDBBtn>
+      </MDBCardBody>
+    </MDBCard>
+    )
   }
 }
 export default GitLab;
