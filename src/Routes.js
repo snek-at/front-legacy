240b193841197e04d4aa32138be6a2df89e63e22
Pinterid <david.pinterics@gmail.com>
2019-11-21 08:41:02 +0100

    Add GitLab Route
    Added the route to access the GitLab-Page.

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,7 @@ import {
   ProfilePage,
   LandingPage,
   RedirectPage,
+  GitLab,
 } from "./components/pages";
 
 class Routes extends React.Component {
@@ -39,6 +40,7 @@ class Routes extends React.Component {
           )}
         />
         <Route exact path="/oauth" component={RedirectPage} />
+        <Route exact path="/gitlab" component={GitLab} />
         <Route
           render={function() {
             return <h1>Not Found</h1>;
