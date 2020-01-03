3d04a09b832212ac7f7228236c318fdd578bb4a6
Aichnerc <contact@aichner-christian.com>
2020-01-03 21:33:31 +0100

    Add register on enter
    The register function is being called when enter is
    pressed on the last password repeat input field.

--- src/components/organisms/sections/Register/index.jsx
+++ src/components/organisms/sections/Register/index.jsx
@@ -139,6 +139,15 @@ class Register extends React.Component{
     });
   }
 
+  logMeIn = (event) => {
+    // 'keypress' event misbehaves on mobile so we track 'Enter' key via 'keydown' event
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation();
+      this.handleSubmit();
+    }
+  }
+
   handleUserNamePick = (username) => {
     this.setState({
       username
@@ -378,6 +387,7 @@ class Register extends React.Component{
           name="password2"
           outline
           value={this.state.password2}
+          onKeyDown={this.logMeIn}
           onChange={this.changeHandler}
           size="lg"
           />
