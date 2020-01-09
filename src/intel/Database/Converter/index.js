ecd158d683f7ca467d42f4bbcfeb816cee2542ec
schettn <nicoschett@icloud.com>
2020-01-09 19:19:05 +0100

    Add repoUrl to converter
    Now the repoUrl is part of a converted repository object.

--- src/intel/Database/Converter/index.js
+++ src/intel/Database/Converter/index.js
@@ -80,6 +80,7 @@ export function getRepositories(data) {
       let repo = {};
       repo.name = repoWithExtras.rName;
       repo.avatarUrl = repoWithExtras.oAvatarUrl;
+      repo.url = repoWithExtras.rUrl;
 
       repo.owner = {};
       repo.owner.name = repoWithExtras.oName;
