c697706e6c795cd62d7ef29e90ea7d27fa6b8213
Aichnerc <contact@aichner-christian.com>
2020-01-15 18:29:29 +0100

    Add ant-design
    The ant-design package has been added.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import React from "react";
 // This serves as an entry point to the DOM and server renderers for React
 import ReactDOM from "react-dom";
 
-//> Font Awesome
-// Font Awesome is an awesome icon library
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-//> Bootstrap
-import "bootstrap-css-only/css/bootstrap.min.css";
-
-//> MDB
-// "Material Design for Bootstrap" is a great UI design framework
-import "mdbreact/dist/css/mdb.css";
+//> Ant Design
+// CSS
+import 'antd/dist/antd.css';
 
 //> CSS
 // Root SCSS file
