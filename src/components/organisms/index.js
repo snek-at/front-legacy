412d34caedcfe63e7c23d587d328541753ea425a
Aichnerc <contact@aichner-christian.com>
2020-01-16 13:47:34 +0100

    Add organism export file
    The uniform export file has been added for the
    organism components.

---
+++ src/components/organisms/index.js
@@ -0,0 +1,14 @@
+import Register from "./Register";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export {
+  Register,
+  Navbar,
+  Footer,
+}
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
