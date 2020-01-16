377ecb74dcc1e103cbd12ad1de818b23535ffcc5
Aichnerc <contact@aichner-christian.com>
2020-01-16 19:52:05 +0100

    Improve footer
    The texts have been changed to English and the
    links have been adjusted.

--- src/components/organisms/Footer/index.jsx
+++ src/components/organisms/Footer/index.jsx
@@ -27,10 +27,10 @@ import Logo from  "../../../assets/navigation/logo.png";
 //> Dynamic texts
 // Slogans
 const slogans = [
-    "Werde eine SNEK!",
-    "Verbinde dich mit deinen KollegInnen!",
-    "Zeige der Welt was du kannst!",
-    "Worauf warten Sie noch?",
+    "Become a snek!",
+    "Connect with your colleges!",
+    "Show the world what you can do!",
+    "What are you waiting for?!",
 ]
 
 class Footer extends React.PureComponent{
@@ -53,14 +53,14 @@ class Footer extends React.PureComponent{
             <MDBFooter color="white text-dark">
                 <MDBRow className="social">
                     <MDBCol md="12" className="text-center">
-                        <h4>Verbinden Sie sich mit uns!</h4>
+                        <h4>Connect with us!</h4>
                     </MDBCol>
                     <MDBCol md="12" className="text-center">
                         <MDBBtn
                         tag="a"
                         floating
                         social="git"
-                        href="https://github.com/aichner"
+                        href="https://github.com/snek-at"
                         rel="noopener noreferrer"
                         target="_blank"
                         >
@@ -70,7 +70,7 @@ class Footer extends React.PureComponent{
                         tag="a"
                         floating
                         social="ins"
-                        href="https://www.instagram.com/aichnerchristian/"
+                        href="https://www.instagram.com/snek-at/"
                         rel="noopener noreferrer"
                         target="_blank"
                         >
@@ -80,7 +80,7 @@ class Footer extends React.PureComponent{
                         tag="a"
                         floating
                         social="fb"
-                        href="https://www.facebook.com/werbeagentur.aichner"
+                        href="https://www.facebook.com/snek-at"
                         rel="noopener noreferrer"
                         target="_blank"
                         >
@@ -89,18 +89,8 @@ class Footer extends React.PureComponent{
                         <MDBBtn
                         tag="a"
                         floating
-                        social="li"
-                        href="https://linkedin.com/in/aichnerc"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                        >
-                            <MDBIcon fab icon="linkedin-in" />
-                        </MDBBtn>
-                        <MDBBtn
-                        tag="a"
-                        floating
                         social="tw"
-                        href="https://twitter.com/realaichner"
+                        href="https://twitter.com/snek-at"
                         rel="noopener noreferrer"
                         target="_blank"
                         >
@@ -109,18 +99,8 @@ class Footer extends React.PureComponent{
                         <MDBBtn
                         tag="a"
                         floating
-                        social="wa"
-                        href="https://api.whatsapp.com/send?phone=4368120502754&text=Hey%2C%20ich%20bin%20interessiert%20an%20einem%20Angebot%20eurer%20Agentur."
-                        rel="noopener noreferrer"
-                        target="_blank"
-                        >
-                            <MDBIcon fab icon="whatsapp" />
-                        </MDBBtn>
-                        <MDBBtn
-                        tag="a"
-                        floating
                         social="email"
-                        href="mailto:info@aichner-christian.com"
+                        href="mailto:snek@aichner-christian.com"
                         >
                             <MDBIcon far icon="envelope" />
                         </MDBBtn>
@@ -128,67 +108,23 @@ class Footer extends React.PureComponent{
                 </MDBRow>
                 <MDBContainer className="text-center text-md-left pt-5">
                     <MDBRow>
-                        <MDBCol md="2">
+                        <MDBCol md="5">
                             <img src={Logo} alt="SNEK Logo" className="img-fluid" />
                             <p className="mt-2">
                             Social Network,<br/>built for engineers.
                             </p>
                         </MDBCol>
                         <MDBCol md="3">
-                            <h5 className="title">Dienste</h5>
+                            <h5 className="title">Useful links</h5>
                             <hr className="agency-red mb-4 mt-0 d-inline-block" />
                             <ul>
-                            <Link to="/service/online-presence">
-                                <li className="list-unstyled">
-                                    <MDBIcon icon="globe" />Online-Präsenz
-                                </li>
-                            </Link>
-                            <Link to="/service/events">
-                                <li className="list-unstyled">
-                                    <MDBIcon icon="handshake" />Events / Messen
-                                </li>
-                            </Link>
-                            <Link to="/service/wedding">
-                                <li className="list-unstyled">
-                                    <MDBIcon icon="ring" />Ihre Hochzeit
-                                </li>
-                            </Link>
-                            <Link to="/service/image">
-                                <li className="list-unstyled">
-                                    <MDBIcon icon="user-tie" />Ihr Image
-                                </li>
-                            </Link>
-                            <Link to="/printing">
-                                <li className="list-unstyled">
-                                    <MDBIcon icon="vector-square" />3D Druck Service
-                                    <MDBBadge pill color="success" className="ml-2">
-                                    New
-                                    </MDBBadge>
-                                </li>
-                            </Link>
-                            </ul>
-                        </MDBCol>
-                        <MDBCol md="3">
-                            <h5 className="title">Nützliche Links</h5>
-                            <hr className="agency-red mb-4 mt-0 d-inline-block" />
-                            <ul>
-                            <Link to="/kisy">
-                                <li className="list-unstyled">
-                                    <MDBIcon icon="user" />Kundenportal
-                                </li>
-                            </Link>
                             <Link to="/faq">
                                 <li className="list-unstyled">
                                     <MDBIcon icon="question" />FAQ
                                 </li>
                             </Link>
-                            <Link to="/blog">
-                                <li className="list-unstyled">
-                                    <MDBIcon icon="rss" />Blog
-                                </li>
-                            </Link>
                             <a 
-                            href="https://github.com/aichner/React-MDB-Template/blob/master/CONTRIBUTING.md"
+                            href="https://github.com/snek-at/front/blob/master/CONTRIBUTING.md"
                             target="_blank"
                             rel="noopener noreferrer"
                             >
@@ -202,44 +138,37 @@ class Footer extends React.PureComponent{
                             rel="noopener noreferrer"
                             >
                                 <li className="list-unstyled">
-                                    <MDBIcon icon="coffee" />Einen Kaffee spendieren
+                                    <MDBIcon icon="coffee" />Get us a coffee
                                 </li>
                             </a>
                             <Link to="/branding">
                                 <li className="list-unstyled">
-                                    <MDBIcon icon="palette" />Presse / Branding
+                                    <MDBIcon icon="palette" />Press / Branding
                                 </li>
                             </Link>
                             </ul>
                         </MDBCol>
                         <MDBCol md="4">
-                            <h5 className="title">Kontakt</h5>
+                            <h5 className="title">Contact</h5>
                             <hr className="agency-red mb-4 mt-0 d-inline-block" />
                             <ul>
-                            <Link to="/location">
+                              <li className="list-unstyled">
+                                  <MDBIcon icon="home" />Emailwerkstraße 29, Carinthia, Austria
+                              </li>
+                            <a href="mailto:snek@aichner-christian.com">
                                 <li className="list-unstyled">
-                                    <MDBIcon icon="home" />Villach, Kärnten, AT
-                                </li>
-                            </Link>
-                            <a href="mailto:info@aichner-christian.com">
-                                <li className="list-unstyled">
-                                    <MDBIcon far icon="envelope" />info@aichner-christian.com
-                                </li>
-                            </a>
-                            <a href="tel:004368120502754">
-                                <li className="list-unstyled">
-                                    <MDBIcon icon="phone" />+43 681 205027 54
+                                    <MDBIcon far icon="envelope" />snek@aichner-christian.com
                                 </li>
                             </a>
                             <hr />
                             <Link to="/about">
                                 <li className="list-unstyled">
-                                    <MDBIcon far icon="file-alt" />Impressum
+                                    <MDBIcon far icon="file-alt" />About
                                 </li>
                             </Link>
                             <Link to="/privacy">
                                 <li className="list-unstyled">
-                                    <MDBIcon icon="balance-scale" />Datenschutzerklärung
+                                    <MDBIcon icon="balance-scale" />Privacy
                                 </li>
                             </Link>
                             <Link to="/terms">
@@ -251,7 +180,7 @@ class Footer extends React.PureComponent{
                         </MDBCol>
                         <MDBCol md="12" className="text-center my-5">
                             <h4>{this.state.slogan}</h4>
-                            <MDBBtn size="lg" rounded color="primary">Kontakt</MDBBtn>
+                            <MDBBtn size="lg" color="green">Join now</MDBBtn>
                         </MDBCol>
                     </MDBRow>
                 </MDBContainer>
@@ -259,14 +188,14 @@ class Footer extends React.PureComponent{
                     <MDBContainer fluid>
                         &copy; 2019 - {new Date().getFullYear()} Copyright: Werbeagentur Christian Aichner
                         <p className="my-2 font-weight-bold madeby">
-                          Made with <MDBIcon 
-                          icon="heart"
-                          className="pulse red-text"
-                          aria-hidden="true"
-                          /> by <a 
-                          href="https://www.aichner-christian.com"
-                          target="_block"
-                          >us</a>.
+                            Made with <MDBIcon 
+                            icon="heart"
+                            className="pulse red-text"
+                            aria-hidden="true"
+                            /> by <a 
+                            href="https://www.aichner-christian.com"
+                            target="_block"
+                            >us</a>.
                         </p>
                     </MDBContainer>
                 </div>
