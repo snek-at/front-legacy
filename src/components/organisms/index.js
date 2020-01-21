421f8b129d70269220a65428b88d8f212d3fe96d
Aichnerc <contact@aichner-christian.com>
2020-01-21 19:01:36 +0100

    Add ProfileContent
    The profile content component has been
    implemented.

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,11 +1,13 @@
 import Register from "./Register";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import ProfileContent from "./ProfileContent";
 
 export {
   Register,
   Navbar,
   Footer,
+  ProfileContent,
 }
 
 /**
