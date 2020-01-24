e1bd6bd26c01bcf99dbe62b8e4bc2b8c8b7db4ab
Aichnerc <contact@aichner-christian.com>
2020-01-24 15:28:43 +0100

    Add Liberapay button
    A new donate button has been added.

--- src/components/molecules/modals/Donate/index.jsx
+++ src/components/molecules/modals/Donate/index.jsx
@@ -12,6 +12,12 @@ import {
   MDBModalFooter,
 } from "mdbreact";
 
+//> Images
+import { ReactComponent as LiberaPayLogo } from '../../../../assets/content/liberapay.svg';
+
+//> CSS
+import "./donate.scss";
+
 class Donate extends React.Component {
   state = {
     visible: false
@@ -27,7 +33,7 @@ class Donate extends React.Component {
     return (
       <>
         <MDBModalHeader
-        className="text-center text-dark" 
+        className="text-center text-dark donate"
         titleClass="w-100"
         tag="p"
         >
@@ -40,18 +46,38 @@ class Donate extends React.Component {
           color="blue"
           href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=84FMCNJ3W46FW&source=url"
           target="_blank"
+          className="d-block"
           >
-          <MDBIcon fab icon="paypal" className="mr-2" />
+          <MDBIcon fab icon="paypal" className="mr-1" />
           PayPal
           </MDBBtn>
           <MDBBtn
           color="elegant"
           href="https://github.com/sponsors/Aichnerc"
           target="_blank"
+          className="d-block"
           >
-          <MDBIcon fab icon="github" className="mr-2" />
+          <MDBIcon fab icon="github" className="mr-1" />
           GitHub Sponsors
           <MDBIcon icon="heart" className="pink-text ml-1" />
+          </MDBBtn>
+          <MDBBtn
+          color="yellow"
+          href="https://liberapay.com/aichner/donate"
+          target="_blank"
+          className="d-block"
+          >
+          <LiberaPayLogo className="liberapay mr-1" />
+          <span className="d-inline">Liberapay</span>
+          </MDBBtn>
+          <MDBBtn
+          color="orange"
+          className="d-block"
+          href="https://liberapay.com/aichner/donate"
+          target="_blank"
+          >
+          <MDBIcon fab icon="btc" className="mr-1" />
+          Bitcoin
           </MDBBtn>
           <div className="w-100">
             <div className="splitter my-2">
@@ -61,6 +87,7 @@ class Donate extends React.Component {
             </div>
           </div>
           <MDBBtn
+          className="d-block"
           color="light"
           href="mailto:contact@aichner-christian.com"
           >
