0ad79b2e87edc15c2ca004eb3045573f22d61a65
Pinterid <david.pinterics@gmail.com>
2020-01-31 09:33:12 +0100

    Updates

--- precache-manifest.7f2e653bd538c478f0cee2e04d2e6220.js
+++ precache-manifest.7f2e653bd538c478f0cee2e04d2e6220.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c2663558643a3bb9c1f10ab53c3fd773",
+    "revision": "34a9ef2224853cb0065ecaee29b9263e",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.9472c03c.chunk.css"
   },
   {
-    "revision": "a2d55bffc8357f14eafe",
+    "revision": "7096677fcea799783aeb",
     "url": "./static/css/main.6a952317.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.38a3e447.chunk.js.LICENSE"
   },
   {
-    "revision": "a2d55bffc8357f14eafe",
-    "url": "./static/js/main.b780e2a5.chunk.js"
+    "revision": "7096677fcea799783aeb",
+    "url": "./static/js/main.f173a462.chunk.js"
   },
   {
     "revision": "242ab2ae145bb7ed8cb0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6091d40453204b02876b046c8fcf81ca.js"
+  "./precache-manifest.7f2e653bd538c478f0cee2e04d2e6220.js"
 );
 
 self.addEventListener('message', (event) => {
