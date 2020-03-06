26816f09c717e2372e139b0b00ebef6f7a61bde5
Pinterid <david.pinterics@gmail.com>
2020-03-06 11:44:16 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7af8a937570c1a950f0f770994238a20.js"
+  "/precache-manifest.3c83544b52983a54b4bd726b0c3d751a.js"
 );
 
 self.addEventListener('message', (event) => {
