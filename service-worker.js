dd811a98f51c1345baaf290d92777baff4cf6768
sxct <nicoschett@icloud.com>
2020-05-06 12:11:29 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.026903a75a31bd8208b1154cf1032561.js"
+  "/precache-manifest.b2010a08c6e74a2df977f92d0ece450c.js"
 );
 
 self.addEventListener('message', (event) => {
