fa008706fa5c36d90f673c60423554dfeaedfe05
Christian Aichner <contact@aichner-christian.com>
2020-05-29 18:37:01 +0200

    Add TalksTab
    The talks tab has been re added.

--- src/components/molecules/modals/index.js
+++ src/components/molecules/modals/index.js
@@ -1,12 +1,9 @@
 //> Components
 // Import all components to export them for easy access from parent components
-import ProfilePage from "./ProfilePage";
-import CompanyPage from "./CompanyPage";
-import HomePage from "./HomePage";
-import TalkPage from "./TalkPage";
+import UploadModal from "./UploadModal";
 
 // Export the components for easy access from parent components
-export { ProfilePage, CompanyPage, HomePage, TalkPage };
+export { UploadModal };
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
--- src/components/organisms/tabs/index.js
+++ src/components/organisms/tabs/index.js
@@ -2,9 +2,10 @@
 // Import all components to export them for easy access from parent components
 import ProjectTab from "./ProjectTab";
 import OverviewTab from "./OverviewTab";
+import TalksTab from "./TalksTab";
 
 // Export the components for easy access from parent components
-export { ProjectTab, OverviewTab };
+export { ProjectTab, OverviewTab, TalksTab };
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
